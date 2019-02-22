934b700688af51e14c2bee5b3b35ecd6ba3357b4
vigneshabi <42170120+vigneshabi@users.noreply.github.com>
2019-02-22 14:17:15 +0530

    Create pn.py

---
+++ pn.py
@@ -0,0 +1,20 @@
+ 
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
+Here, we have used the if…elif…else statement. We can do the same thing using nested if statements as follows.
+
+# This time use nested if to solve the problem
+ 
+num = float(input("Enter a number: "))
+if num >= 0:
+   if num == 0:
+       print("Zero")
+   else:
+       print("Positive number")
+else:
+   print("Negative number")
