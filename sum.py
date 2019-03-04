d3025b919aa847175afe7888cceec1bca15d6109
vigneshabi <42170120+vigneshabi@users.noreply.github.com>
2019-03-04 14:47:00 +0530

    sum.py

---
+++ sum.py
@@ -0,0 +1,11 @@
+num = 16
+num = int(input("Enter a number: "))
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   while loop to iterate un till zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is",sum)
