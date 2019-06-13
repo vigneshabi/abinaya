11da0df288e9bc6320899f7031f40a284f53f63e
vigneshabi <42170120+vigneshabi@users.noreply.github.com>
2019-06-13 11:14:08 +0530

    Update range.py

--- range.py
+++ range.py
@@ -1,5 +1,7 @@
-n=int(input())
-if i in range(1<10):
-  print("yes")
-else:
-  print("no")
+def test_range(n):
+    if n in range(0,10):
+        print( " %s is in the range"%str(n))
+    else :
+        print("The number is inside the given range.")
+        print("yes")
+test_range(9)
